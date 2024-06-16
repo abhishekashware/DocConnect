dad1ca2d36586070b4baf7b6d99a48c34a63785c
Abhishek Ashware <abhishekashware12517@gmail.com>
2024-06-17 00:03:06 +0530

    changed

--- api/client/src/screens/UserAppointmentScreen/index.jsx
+++ api/client/src/screens/UserAppointmentScreen/index.jsx
@@ -92,7 +92,7 @@ const UserAppointment = () => {
                                             }}
                                             class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"
                                         >
-                                            Reject
+                                            Cancel
                                         </button>
                                     </td>
                                 </tr>
